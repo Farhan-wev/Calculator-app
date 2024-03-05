b0b637d8ce0c0c2b92669a049f4e3261e3aeebfd
Farhan <farhanjuliansya@students.amikom.ac.id>
2024-03-05 12:07:04 +0700

    enambahkan kode

---
+++ Calculator.cs
@@ -0,0 +1,18 @@
+  class calculator
+  {
+	  static void Main(string[] args)
+	  {
+		  
+
+	  }
+  }
+  static int Penambahan(int a, int b)
+  {
+	  return a + b;
+  }
+  static int Pengurangan(int a, int b)
+  {
+	  return a - b;
+  }
+  
+	  
